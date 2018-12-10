54e1378ddb987eddf5ced77a4057dedfea004e28
benjamin <benjamin@benbook2018>
2018-12-09 22:49:35 -0800

    Now more random realistic data!

--- ChristmasTreeInfo/src/christmastreeinfo/Main.java
+++ ChristmasTreeInfo/src/christmastreeinfo/Main.java
@@ -2,8 +2,14 @@ package christmastreeinfo;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 import benjaminc.util.TracedPrintStream;
 import customer.Customer;
@@ -24,17 +30,16 @@ public class Main {
 		System.out.println(ben.get(DataType.PHONE_NUMBER));
 		System.out.println(ben.get(DataType.ADDRESS));
 		List<Customer> cal = new ArrayList<Customer>();
+		int count = 7;
 		cal.add(ben);
-		Scanner s;
 		try {
-			s = new Scanner(new File("people.txt"));
-			for(int i = 0; i < 7; i++) {
-				cal.add(Customer.getCustomerFromString(s.nextLine(), ";", DataType.NAME, DataType.PHONE_NUMBER,
-						DataType.EMAIL_ADDRESS, DataType.ADDRESS, DataType.CITY));
+			PersonFactory pf = new PersonFactory(new File("people.txt"));
+			for(int i = 0; i < count; i++) {
+				cal.add(pf.getCustomer());
 			}
 		} catch (FileNotFoundException e) {
 			System.out.println("FileNotFoundException, using backup generation method");
-			for(int i = 0; i < 74; i++) {
+			for(int i = 0; i < count; i++) {
 				cal.add(Customer.getRandomCustomer());
 			}
 		}
@@ -44,4 +49,43 @@ public class Main {
 				
 	}
 
+	@Deprecated
+	private List<Customer> oldGeneration(int pplcount) {
+		List<Customer> cal = new ArrayList<Customer>();
+		Path path = Paths.get("people.txt");
+		int lineCount = 0;
+		try {
+			lineCount = (int) Files.lines(path).count();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		int locs[] = new int[pplcount];
+		HashMap<Integer, Customer> ppl = new HashMap<Integer, Customer>();
+		Random r = new Random();
+		while(ppl.size() < pplcount) {
+			int spt = r.nextInt(lineCount);
+			if(!ppl.containsKey(spt)) {
+				ppl.put(spt, null);
+			}
+		}
+		Scanner s;
+		try {
+			s = new Scanner(new File("people.txt"));
+			for(int i = 0; i < lineCount; i++) {
+				if(ppl.containsKey(i)) {
+					cal.add(Customer.getCustomerFromString(s.nextLine(), ";", DataType.NAME, DataType.PHONE_NUMBER,
+							DataType.EMAIL_ADDRESS, DataType.ADDRESS, DataType.CITY));
+				}
+				if(cal.size() == pplcount) {
+					break;
+				}
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("FileNotFoundException, using backup generation method");
+			for(int i = 0; i < 74; i++) {
+				cal.add(Customer.getRandomCustomer());
+			}
+		}
+		return cal;
+	}
 }
---
+++ ChristmasTreeInfo/src/christmastreeinfo/PersonFactory.java
@@ -0,0 +1,76 @@
+package christmastreeinfo;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+import customer.Customer;
+import customer.DataType;
+
+public class PersonFactory {
+	
+	private List<String> name1;
+	private List<String> name2;
+	private List<String> phone;
+	private List<String> email;
+	private List<String> address;
+	private List<String> city;
+	private Scanner scan;
+	private int filesize;
+	
+	public PersonFactory(File f) throws FileNotFoundException {
+		Path path = Paths.get(f.getPath());
+		int lineCount = 0;
+		try {
+			lineCount = (int) Files.lines(path).count();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		filesize = lineCount;
+		
+		name1 = new ArrayList<String>();
+		name2 = new ArrayList<String>();
+		phone = new ArrayList<String>();
+		email = new ArrayList<String>();
+		address = new ArrayList<String>();
+		city = new ArrayList<String>();
+		
+		scan = new Scanner(f);
+		for(int i = 0; i < filesize; i++) {
+			String part[] = scan.nextLine().split(";");
+			switch(part.length) {
+			case 5: city.add(part[4]);
+			case 4: address.add(part[3]);
+			case 3: email.add(part[2]);
+			case 2: phone.add(part[1]);
+			case 1: {String name[] = part[0].split(" ");
+				System.out.println(name.length);
+				name1.add(name[0]);
+				if(name.length >= 2) {
+					name2.add(name[1]);
+				}
+			}
+			}
+		}
+		scan.close();
+	}
+	
+	public Customer getCustomer() {
+		Customer out = new Customer();
+		Random r = new Random();
+		out.set(DataType.NAME, name1.get(r.nextInt(name1.size())) + " " + name2.get(r.nextInt(name2.size())));
+		out.set(DataType.PHONE_NUMBER, phone.get(r.nextInt(phone.size())));
+		out.set(DataType.EMAIL_ADDRESS, email.get(r.nextInt(email.size())));
+		out.set(DataType.ADDRESS, address.get(r.nextInt(address.size())));
+		out.set(DataType.CITY, city.get(r.nextInt(city.size())));
+		return out;
+	}
+
+}
